9c0b233b05a7f05df89ba716174abd16dfadf82e
John Costales <j.g.costales96@gmail.com>
2018-11-18 23:45:28 +0000

    Messing with pixels

--- CA1/Code/sketch.js
+++ CA1/Code/sketch.js
@@ -1,10 +1,24 @@
 function setup() {
-
+  createCanvas(800, 600);
+  colorMode(HSB);
+  background(55);
+  pixelDensity(1);
 }
 
 function draw() {
+  loadPixels();
 
-    
+  for (let x = 0; x <= width; x++ ) {
+    for (let y = 0; y <= height; y++) {
+      var index = (x + y * width) * 4;
+      pixels[index + 0] = x;
+      pixels[index + 1] = random(255);
+      pixels[index + 2] = y;
+      pixels[index + 3] = 255;
+    }
+  }
+      updatePixels();
+
 }
 
 function keyPressed() {
