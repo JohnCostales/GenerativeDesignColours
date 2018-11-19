f9552bb47b8208b113680574acb4b47b4f513c31
John Costales <j.g.costales96@gmail.com>
2018-11-19 12:51:24 +0000

    Control the scaling pattern and shape

--- CA1/Code/sketch.js
+++ CA1/Code/sketch.js
@@ -1,27 +1,55 @@
 var n = 0; // Ordering number
-var c = 8; // Scaling parameter
+var c = 0; // Scaling parameter
+var angle = 0;
+var divergenceAngle = 137.5;
+var shapeSize = 1;
+
+
 
 function setup() {
   createCanvas(800, 800);
   angleMode(DEGREES); // Change angles to degrees
-  colorMode(HSB);
-  background(0);
+  colorMode(HSB, 360, 100, 100, 100);
+  noStroke();
+  background(255);
 }
 
 function draw() {
 
-  var angle = n * 137.3;
+  angle = n * divergenceAngle;
   var radius = c * sqrt(n);
 
+  var shapeFill = color(n % 255, 255, 255);
   //Radius against an angle
-  var x = radius * cos(angle) + width/2;
-  var y = radius * sin(angle) + height/2;
+  x = radius * cos(angle) + width/2;
+  y = radius * sin(angle) + height/2;
 
-  fill(angle % 256, 255, 255);
-  ellipse(x,y,8,8);
-  n++
+  fill(shapeFill);
+  // ellipse(x ,y ,shapeSize ,shapeSize);
+
+  console.log("Radius: " + radius);
+  console.log("N: " + n);
+
+  // if every sqrt(n) is a natural number. grow scaling.
+  //Change colour alpha?
+  if (Number.isInteger(sqrt(n)) === true) {
+    shapeSize++;
+    c+=1;
+  }
+
+  n++;
+  // noLoop();
 }
+
 
 function keyPressed() {
         if(key=='s' || key=='S') saveCanvas(gd.timestamp() + '_MouseX' + mouseX + '_MouseY' + mouseY,'png');
+
+        switch (key) {
+          case '1':
+            // Reset canvas variables and change
+            break;
+          default:
+
+        }
     }
