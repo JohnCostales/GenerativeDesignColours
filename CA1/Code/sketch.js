86409cba33a12b1d403fbcd1cb90dcee8ad78ba9
John Costales <j.g.costales96@gmail.com>
2018-11-19 14:08:00 +0000

    Phyllotaxis function

--- CA1/Code/sketch.js
+++ CA1/Code/sketch.js
@@ -1,46 +1,47 @@
 var n = 0; // Ordering number
-var c = 0; // Scaling parameter
+var c = 8; // Scaling parameter
 var angle = 0;
 var divergenceAngle = 137.5;
-var shapeSize = 1;
-
-
+var shapeSize = 8;
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1240, 1748);
   angleMode(DEGREES); // Change angles to degrees
   colorMode(HSB, 360, 100, 100, 100);
   noStroke();
-  background(255);
+  background(0);
 }
 
 function draw() {
-
   angle = n * divergenceAngle;
-  var radius = c * sqrt(n);
-
   var shapeFill = color(n % 255, 255, 255);
-  //Radius against an angle
-  x = radius * cos(angle) + width/2;
-  y = radius * sin(angle) + height/2;
 
+  phyllotaxis();
+
+  // Draw shapes
   fill(shapeFill);
-  // ellipse(x ,y ,shapeSize ,shapeSize);
-
-  console.log("Radius: " + radius);
-  console.log("N: " + n);
+  ellipse(x ,y ,shapeSize ,shapeSize);
 
   // if every sqrt(n) is a natural number. grow scaling.
   //Change colour alpha?
   if (Number.isInteger(sqrt(n)) === true) {
-    shapeSize++;
-    c+=1;
+    // shapeSize++;
+    // c+=1;
+    console.log("N: " + n);
   }
 
   n++;
   // noLoop();
 }
 
+function phyllotaxis() {
+  var r = c * sqrt(n);
+
+  //Radius against an angle
+  x = r * cos(angle) + width/2;
+  y = r * sin(angle) + height/2;
+  console.log("Radius: " + r);
+}
 
 function keyPressed() {
         if(key=='s' || key=='S') saveCanvas(gd.timestamp() + '_MouseX' + mouseX + '_MouseY' + mouseY,'png');
@@ -48,6 +49,7 @@ function keyPressed() {
         switch (key) {
           case '1':
             // Reset canvas variables and change
+            setup();
             break;
           default:
 
