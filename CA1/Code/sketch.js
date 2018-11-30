377f987906975e761e57b4fc12d15eb742e60285
John Costales <j.g.costales96@gmail.com>
2018-11-30 00:59:05 +0000

    Class romoved. perlin noise added

--- CA1/Code/sketch.js
+++ CA1/Code/sketch.js
@@ -1,58 +1,62 @@
 var divergenceAngle = 137.5;
-var c = 10;
-var shapeSize = 8;
-
-var dim;
+var c = 24;
+var shapeSize = c-1;
+//var hV = 360;
+var sV = 100;
+var bV = 100;
+var aV = 1;
 
 function setup() {
     createCanvas(1240, 1748);
     angleMode(DEGREES); // Change angles to degrees
-    colorMode(HSB, 360, 100, 100, 100);
+    colorMode(HSB);
     noStroke();
-    background(0);
 }
 
 function draw() {
     background(0);
-    noLoop();
+    translate(width / 1.5, height / 1.5);
 
-    var hueValue = 255;
-    var saturationValue = 100;
-    var brightnessValue = 100;
     for (var n = 0; n < width; n++) {
-
+        
+        
         var angle = n * divergenceAngle;
         var r = c * sqrt(n);
+
         //Radius against an angle
-        var x = r * cos(angle) + width / 1.5;
-        var y = r * sin(angle) + height / 1.5;
+        var x = r * cos(angle);
+        var y = r * sin(angle);
 
         // Draw shapes
-        //        var shapeFill = color((angle - r) % 150, saturation, brightness);
-        drawEllipse(x, y, n, hueValue, saturationValue, brightnessValue);
+        var hV = noise(n * .01) * 360;
+        var shapeFill = color(hV, sV, bV, aV);
+        //        var shapeFill = color(n % hV, sV, bV, aV);
+        fill(shapeFill);
+        ellipse(x, y, shapeSize, shapeSize);
 
         // if every sqrt(n) is a natural number. grow scaling.
         //Change colour alpha?
         if (Number.isInteger(sqrt(n)) === true) {
-            shapeSize += 2;
-            c += 1;
+            aV -= 0.02;
         }
         if (Number.isInteger(Math.pow(n, 3)) === true) {
-            brightnessValue -= 0.15;
+
         }
         //        console.log(y);
     }
-
-    noLoop();
+            noLoop();
 }
 
-function drawEllipse(x, y, n, hV, sV, bV) {
-    var shapeFill = color(n % hV, sV, bV);
-    fill(shapeFill);
-    ellipse(x, y, shapeSize, shapeSize);
-}
+
 
 function keyPressed() {
     if (key == 's' || key == 'S') saveCanvas(gd.timestamp() + '_MouseX' + mouseX + '_MouseY' + mouseY, 'png');
 
+    switch (key) {
+        case '1': // Reset canvas variables and change
+            setup();
+            break;
+        default:
+
+    }
 }
